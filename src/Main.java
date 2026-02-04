31b289d347e639723139c21b1d430e68913d8863
JuanCarlosCarbajosaGarcia <juanc.cargar@educa.jcyl.es>
2026-02-04 13:24:27 +0100

    V0.1

---
+++ src/Main.java
@@ -0,0 +1,32 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = 0;
+
+        System.out.println("ingrese el numero de lineas: ");
+        N = sc.nextInt();
+
+        try(BufferedWriter writer = new BufferedWriter(new FileWriter("salida.txt"))){
+            int lineas = 1;
+
+            while(lineas < N){
+                writer.write(lineas + " ");
+                writer.newLine();
+                lineas++;
+            }
+
+
+
+
+
+
+        }catch (IOException e){
+            System.out.println("error: " + e.getMessage());
+        }
+    }
+}
